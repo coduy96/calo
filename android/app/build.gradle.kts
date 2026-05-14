04a1cec316590adb3c6dd2c03b6d1f63f3fcdf55
Apoorv Darshan <ad13dtu@gmail.com>
2026-05-14 15:36:53 +0530

    Bump Android 1.2 build code

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         applicationId = "com.apoorvdarshan.calorietracker"
         minSdk = 26
         targetSdk = 36
-        versionCode = 17
+        versionCode = 18
         versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
